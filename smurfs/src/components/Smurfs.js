85432bc6b406f0687e034ba9804fc5e40f72ce06
CScori <amilanotte1366@gmail.com>
2019-10-11 10:31:01 -0500

    adds smurf store to smurfs

--- smurfs/src/components/Smurfs.js
+++ smurfs/src/components/Smurfs.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import { fetchData } from './actions'
+import Smurf from './Smurf'
 
-const Smurfs = () => {
+
+const Smurfs = (props) => {
+    useEffect(() => {
+        props.fetchData()
+    }, [])
+
     return (
         <div>
-            
+            <Smurf />
         </div>
     )
 }
 
-export default Smurfs
+const mapStateToProps = state => {
+    console.log('Smurf state', state)
+    return{
+        smurfs: state.smurfs,
+        isFetching: state.isFetching,
+        error: state.error
+    }
+}
+
+export default connect(mapStateToProps, {fetchData})(Smurfs)
