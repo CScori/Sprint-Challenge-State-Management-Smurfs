0bd2abdc17e12ac1c1c50066d32a519cc015001a
CScori <amilanotte1366@gmail.com>
2019-10-11 12:49:41 -0500

    created delete actions

--- smurfs/src/components/actions/index.js
+++ smurfs/src/components/actions/index.js
@@ -6,7 +6,8 @@ export const FETCH_FAILURE = 'FETCH_FAILURE'
 export const ADDING_SMURF = "ADDING_SMURF";
 export const ADDING_SMURF_SUCCESS = "ADDING_SMURF_SUCCESS";
 export const ADDING_SMURF_FAILED = "ADDING_SMURF_FAILED";
-
+export const DELETE_SMURF = 'DELETE_SMURF';
+export const DELETE_FAILED = 'DELETE_FAILED';
 export const fetchData = () => dispatch => {
     dispatch({ type: START_FETCH});
     axios
@@ -27,4 +28,15 @@ export const addSmurf = (newSmurf) => dispatch => {
         dispatch({ type: ADDING_SMURF_SUCCESS, payload: res.data})
     })
     .catch(err => dispatch({ type: ADDING_SMURF_FAILED, payload: err.response}))
+}
+
+export const deleteSmurf = (id) => dispatch => {
+    dispatch({ type: DELETE_SMURF});
+    axios
+    .delete('http://localhost:3333/smurfs')
+    .then(res => {
+        console.log('minus smurf array', res.data)
+        dispatch({ type: DELETE_SMURF, payload: res.data.id})
+    })
+    .catch(err => dispatch({ type: DELETE_FAILED }))  
 }
