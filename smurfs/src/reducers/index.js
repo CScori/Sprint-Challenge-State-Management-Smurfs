bb6d7357cebb00a0b1a70618feedc07f33536795
CScori <amilanotte1366@gmail.com>
2019-10-11 09:51:24 -0500

    adds the state and initial reducer

---
+++ smurfs/src/reducers/index.js
@@ -0,0 +1,12 @@
+const initialState = {
+    smurfs: [],
+    isFetching: false,
+    error: 'something went wrong'
+}
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+export default reducer
