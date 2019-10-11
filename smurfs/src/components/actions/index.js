91769b0245e1cf4b1c2d501c5838c26cbae584a7
CScori <amilanotte1366@gmail.com>
2019-10-11 10:12:54 -0500

    adds axios function

--- smurfs/src/components/actions/index.js
+++ smurfs/src/components/actions/index.js
@@ -3,3 +3,14 @@ import axios from 'axios'
 export const START_FETCH = 'START_FETCH'
 export const FETCH_SUCCESS = 'FETCH_SUCCESS'
 export const FETCH_FAILURE = 'FETCH_FAILURE'
+
+export const fetchData = () => dispatch => {
+    dispatch({ type: START_FETCH});
+    axios
+    .get('')
+    .then(res => {
+        console.log('fetch data', res)
+        dispatch({ type: FETCH_SUCCESS, payload: res})
+    })
+    .catch(err => dispatch({ type: FETCH_FAILURE, payload: err.response}))
+}
