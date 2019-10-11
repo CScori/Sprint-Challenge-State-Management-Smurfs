359217de1f899549d10d4a683e36c4de4fa83c6c
CScori <amilanotte1366@gmail.com>
2019-10-11 11:39:20 -0500

    setting up add smurf

--- smurfs/src/components/AddSmurf.js
+++ smurfs/src/components/AddSmurf.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { connect } from 'react-redux';
+import {addSmurf} from './actions'
 
-const AddSmurf = () => {
+const AddSmurf = (props) => {
+    const [smurfName, setSmurfName] = useState('')
+    const [smurfAge, setSmurfAge] = useState('')
+    const [smurfHeight, setSmurfHeight] = useState('')
+    const [newSmurf, SetNewSmurf] = useState({})
+
+    useEffect(() => {
+        props.addSmurf()
+  
+    }, [])
+    SetNewSmurf(smurfName, smurfAge, smurfHeight)
     return (
         <div>
-            
+            <form>
+                <input
+                    name='name'
+                    placeholder='smurf name'
+                    value={smurfName}
+                    onChange={e => setSmurfName(e.target.value)}
+                />
+                <input
+                    name='age'
+                    placeholder='smurf age'
+                    value={smurfAge}
+                    onChange={e => setSmurfAge(e.target.value)}
+                />
+                <input
+                    name='height'
+                    placeholder='smurf height'
+                    value={smurfHeight}
+                    onChange={e => setSmurfHeight(e.target.value)}
+                />
+                <button onClick={e => props.addSmurf(newSmurf)}>Add Smurfs</button>
+            </form>
         </div>
     )
 }
 
-export default AddSmurf
+export default connect(null, {addSmurf})(AddSmurf)
--- smurfs/src/components/actions/index.js
+++ smurfs/src/components/actions/index.js
@@ -3,6 +3,9 @@ import axios from 'axios'
 export const START_FETCH = 'START_FETCH'
 export const FETCH_SUCCESS = 'FETCH_SUCCESS'
 export const FETCH_FAILURE = 'FETCH_FAILURE'
+export const ADDING_SMURF = "ADDING_SMURF";
+export const ADDING_SMURF_SUCCESS = "ADDING_SMURF_SUCCESS";
+export const ADDING_SMURF_FAILED = "ADDING_SMURF_FAILED";
 
 export const fetchData = () => dispatch => {
     dispatch({ type: START_FETCH});
@@ -13,4 +16,15 @@ export const fetchData = () => dispatch => {
         dispatch({ type: FETCH_SUCCESS, payload: res.data})
     })
     .catch(err => dispatch({ type: FETCH_FAILURE, payload: err.response}))
+}
+
+export const addSmurf = (newSmurf) => dispatch => {
+    dispatch({ type: ADDING_SMURF});
+    axios
+    .get('http://localhost:3333/smurfs/POST')
+    .then(res => {
+        console.log('fetch data', res)
+        dispatch({ type: ADDING_SMURF_SUCCESS, payload: res})
+    })
+    .catch(err => dispatch({ type: ADDING_SMURF_FAILED, payload: err.response}))
 }
